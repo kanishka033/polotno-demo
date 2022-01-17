24fe026c205789af0db0945a03dcb441b9ab53b2
kanishka suvarna <kanishkasuvarna2001@gmail.com>
2022-01-17 20:56:02 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
-
+import { createDemoApp } from 'polotno/polotno-app';
 function App() {
+
+  useEffect(() => {
+    const  store = createDemoApp({
+      container: document.getElementById("App"),
+      key: "Jq19lz43FJSa7QAkVGmM"
+    });
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <> 
+      <h4> Design Center </h4> 
+      <div id="App" >
+      </div>
+    </>
   );
 }
 
