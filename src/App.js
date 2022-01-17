0cba93536fd2f0038819e86b3659b2b6e36642e2
kanishka suvarna <kanishkasuvarna2001@gmail.com>
2022-01-17 21:54:27 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import './App.css';
-import { createDemoApp } from 'polotno/polotno-app';
-function App() {
 
-  useEffect(() => {
-    const  store = createDemoApp({
-      container: document.getElementById("App"),
-      key: "Jq19lz43FJSa7QAkVGmM"
-    });
-  }, [])
+import { PolotnoContainer, SidePanelWrap, WorkspaceWrap } from 'polotno';
+import { Toolbar } from 'polotno/toolbar/toolbar';
+import { ZoomButtons } from 'polotno/toolbar/zoom-buttons';
+import { SidePanel } from 'polotno/side-panel';
+import { Workspace } from 'polotno/canvas/workspace';
 
+
+export const App = ({ store }) => {
   return (
-    <> 
-      <h4> Design Center </h4> 
-      <div id="App" >
-      </div>
+    <>
+    <h4> Desigma </h4>
+    <PolotnoContainer style={{ width: '100vw', height: 'calc(100vh - 56px)' }}>
+      <SidePanelWrap>
+        <SidePanel store={store} />
+      </SidePanelWrap>
+      <WorkspaceWrap>
+        <Toolbar store={store} downloadButtonEnabled />
+        <Workspace store={store} />
+        <ZoomButtons store={store} />
+      </WorkspaceWrap>
+    </PolotnoContainer>
     </>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createStore } from 'polotno/model/store';
+
+const store = createStore({
+  key: 'Jq19lz43FJSa7QAkVGmM',
+  showCredit: true
+});
+
+const page = store.addPage();
+
+page.addElement({
+  x: 500,
+  y: 500,
+  type: 'text',
+  fill: 'black',
+  text: 'hello',
+  fontSize: 40,
+  align: 'center'
+});
 
 ReactDOM.render(
-    <App />,
+    <App store={store} />,
   document.getElementById('root')
 );
 
